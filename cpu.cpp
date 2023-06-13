e0cc0047b7645e43cc655ad808ccf241d14c5bfc
Stephen Robinson <stefonzo@Stephens-MacBook-Pro.local>
2023-06-13 05:29:56 -0500

    adds helper functions and more instructions

--- cpu.cpp
+++ cpu.cpp
@@ -1,15 +1,70 @@
 #include "cpu.hpp"
 
+void mos6502::ZeroCheck(uint16_t value) {
+    if (value == 0) {
+        flag |= Z;
+    } else {
+        flag &= ~(Z);
+    }
+}
+void mos6502::CarryCheck(uint16_t value) {
+    if (value & 0xFF00) {
+        flag |= C;
+    } else {
+        flag &= ~(C);
+    }
+}
+void mos6502::OverflowCheck(uint16_t value){
+    if (((uint8_t)value & N) ^ (a & N)) {
+        flag |= V;
+    } else {
+        flag &= ~(V);
+    }
+}
+void mos6502::SignCheck(uint16_t value) {
+    if (value & 0x0080) {
+        flag |= N;
+    } else {
+        flag &= ~(N);
+    }
+}
 void mos6502::ADC_IMMEDIATE() {
     uint16_t address = pc++;
     uint8_t m = read(address);
+    uint8_t c = flag & C;
+    uint16_t sum = a + m + c;
+    CarryCheck(sum);
+    ZeroCheck(sum);
+    SignCheck(sum);
+    OverflowCheck(sum);
+    a = (uint8_t)sum;
     cycles_count += 2;
 }
 void mos6502::ADC_ZEROPAGE() {
-    
+    uint16_t address = (uint16_t)read(pc++);
+    uint8_t m = read(address);
+    uint8_t c = flag & C;
+    uint16_t sum = a + m + c;
+    CarryCheck(sum);
+    ZeroCheck(sum);
+    SignCheck(sum);
+    OverflowCheck(sum);
+    a = (uint8_t)sum;
+    cycles_count += 2;
 }
 void mos6502::ADC_ABSOLUTE() {
-    
+    uint8_t la = read(pc++);
+    uint8_t ha = read(pc++);
+    uint16_t address = (uint16_t(ha) << 8) | la;
+    uint8_t m = read(address);
+    uint8_t c = flag & C;
+    uint16_t sum = a + m + c;
+    CarryCheck(sum);
+    ZeroCheck(sum);
+    SignCheck(sum);
+    OverflowCheck(sum);
+    a = (uint8_t)sum;
+    cycles_count += 4;
 }
 void mos6502::ADC_X_ABSOLUTE() {
     
@@ -24,6 +79,15 @@ void mos6502::ADC_X_ZEROPAGE_INDIRECT() {
     
 }
 void mos6502::ADC_Y_ZEROPAGE_INDIRECT() {
+    
+}
+void mos6502::BCC() {
+    
+}
+void mos6502::BCS() {
+    
+}
+void mos6502::BEQ() {
     
 }
 void mos6502::CLC() {
@@ -74,17 +138,47 @@ void mos6502::DEY() {
     }
     cycles_count += 2;
 }
+void mos6502::INX() {
+    x += 1;
+    if (x & N) {
+        flag |= N;
+    } else {
+        flag &= ~(N);
+    }
+    //check Z flag
+    if (x == 0x00) {
+        flag |= Z;
+    } else {
+        flag &= ~(Z);
+    }
+    cycles_count += 2;
+}
+void mos6502::INY() {
+    y += 1;
+    if (y & N) {
+        flag |= N;
+    } else {
+        flag &= ~(N);
+    }
+    //check Z flag
+    if (y == 0x00) {
+        flag |= Z;
+    } else {
+        flag &= ~(Z);
+    }
+    cycles_count += 2;
+}
 void mos6502::JMP_ABSOLUTE() {
     uint8_t la = read(pc++);
     uint8_t ha = read(pc++);
-    uint16_t address = (uint16_t(ha) << 8) | la; //ask about (casting syntax...)
+    uint16_t address = (uint16_t(ha) << 8) | la;
     pc = address;
     cycles_count += 3;
 }
 void mos6502::JMP_ABSOLUTE_INDIRECT() {
     uint8_t la = read(pc++);
     uint8_t ha = read(pc++);
-    uint16 jmp_address = (uint16_t(ha) << 8) | la;
+    uint16_t jmp_address = (uint16_t(ha) << 8) | la;
     la = read(jmp_address++);
     ha = read(jmp_address);
     jmp_address = (uint16_t(ha) << 8) | la;
@@ -92,13 +186,32 @@ void mos6502::JMP_ABSOLUTE_INDIRECT() {
     cycles_count += 5;
 }
 void mos6502::LDA_IMMEDIATE() {
-    
+    uint16_t address = pc++;
+    a = read(address);
+    cycles_count += 2;
 }
 void mos6502::LDA_ABSOLUTE() {
-    
+    uint8_t la = read(pc++);
+    uint8_t ha = read(pc++);
+    uint16_t address = (uint16_t(ha) << 8) | la;
+    a = read(address);
+    cycles_count += 4;
 }
 void mos6502::LDA_ZEROPAGE() {
-    
+    uint16_t address = (uint16_t)read(pc++);
+    a = read(address);
+    cycles_count += 3;
+}
+void mos6502::NOP() {
+    cycles_count += 2;
+}
+void mos6502::PHA() {
+    write(SB + sp--, a);
+    cycles_count += 3;
+}
+void mos6502::PHP() {
+    write(SB + sp--, a);
+    cycles_count += 3;
 }
 void mos6502::InitializeOpcodeTable() {
     opcodeTable[0x69] = &mos6502::ADC_IMMEDIATE;
@@ -109,17 +222,25 @@ void mos6502::InitializeOpcodeTable() {
     opcodeTable[0x75] = &mos6502::ADC_X_ZEROPAGE;
     opcodeTable[0x61] = &mos6502::ADC_X_ZEROPAGE_INDIRECT;
     opcodeTable[0x71] = &mos6502::ADC_Y_ZEROPAGE_INDIRECT;
+    opcodeTable[0x90] = &mos6502::BCC;
+    opcodeTable[0xB0] = &mos6502::BCS;
+    opcodeTable[0xF0] = &mos6502::BEQ;
     opcodeTable[0x18] = &mos6502::CLC;
     opcodeTable[0xD8] = &mos6502::CLD;
     opcodeTable[0x58] = &mos6502::CLI;
     opcodeTable[0xB8] = &mos6502::CLV;
     opcodeTable[0xCA] = &mos6502::DEX;
     opcodeTable[0x88] = &mos6502::DEY;
+    opcodeTable[0xE8] = &mos6502::INX;
+    opcodeTable[0xC8] = &mos6502::INY;
     opcodeTable[0x4C] = &mos6502::JMP_ABSOLUTE;
     opcodeTable[0x6C] = &mos6502::JMP_ABSOLUTE_INDIRECT;
     opcodeTable[0xA9] = &mos6502::LDA_IMMEDIATE;
     opcodeTable[0xAD] = &mos6502::LDA_ABSOLUTE;
     opcodeTable[0xA5] = &mos6502::LDA_ZEROPAGE;
+    opcodeTable[0xEA] = &mos6502::NOP;
+    opcodeTable[0x48] = &mos6502::PHA;
+    opcodeTable[0x08] = &mos6502::PHP;
     
 }
 
--- cpu.hpp
+++ cpu.hpp
@@ -5,19 +5,29 @@
 #include <unordered_map>
 
 #define NES6502
+// Please check out the following code/resource, been my main reference so far
+// rubbermallet.org/fake6502.c
+// https://www.pagetable.com/c64ref/6502/
 
-//define constants for status flag?
-//code is inspired froma github project that I need to properly source (identical constructor)
-//some code for class comes from chatgpt, specifically
+/*
+    TODO:
+        -put flag code into functions (can I cast x & y registers to input for helper functions?)
+        -figure out test to see if page has been crossed in memory access
+        -finish instructions
+        -redo cycles (for now I think its fine but I need to do more research so I can figure out if it will be suitable for timing purposes)
+        -testing
+ */
 
+//stack is page 1
+#define SB 0x01FF // (SB = stack base)
 //Flag masks
-#define N 0x80 //negative
-#define V 0x40 //overflow
-#define B 0x10 //break
-#define D 0x8 //decimal mode
-#define I 0x4 //interrupt
-#define Z 0x2 //zero
-#define C 0x1 //carry
+#define N 0x80 // negative
+#define V 0x40 // overflow
+#define B 0x10 // break
+#define D 0x08 // decimal mode
+#define I 0x04 // interrupt
+#define Z 0x02 // zero
+#define C 0x01 // carry
 
 class mos6502 {
 private:
@@ -29,6 +39,10 @@ private:
     void (*write)(uint16_t, uint8_t);
     typedef void (mos6502::*InstrMethod)(); //function pointer for jump table
     std::unordered_map<uint8_t, InstrMethod> opcodeTable;
+    void ZeroCheck(uint16_t value);
+    void CarryCheck(uint16_t value);
+    void OverflowCheck(uint16_t value);
+    void SignCheck(uint16_t value);
                                     //Assembly:       Opcode   Cycles   Bytes
     void InitializeOpcodeTable();   //-----------------------------------------
     void ADC_IMMEDIATE();           //ADC #$nn        $69      2        2
@@ -39,17 +53,26 @@ private:
     void ADC_X_ZEROPAGE();          //ADC $nn, x      $75      4        2
     void ADC_X_ZEROPAGE_INDIRECT(); //ADC ($nn, x)    $61      6        2
     void ADC_Y_ZEROPAGE_INDIRECT(); //ADC ($nn), y    $71      5+p      2
+    void BCC();                     //BCC $nnnn       $90      2+t+p    2
+    void BCS();                     //BCS $nnnn       $B0      2+t+p    2
+    void BEQ();                     //BEQ $nnnn       $F0      2+t+p    2
     void CLC();                     //CLC             $18      2        1
     void CLD();                     //CLD             $D8      2        1
     void CLI();                     //CLI             $58      2        1
     void CLV();                     //CLV             $B8      2        1
     void DEX();                     //DEX             $CA      2        1
     void DEY();                     //DEY             $88      2        1
+    void INX();                     //INX             $E8      2        1
+    void INY();                     //INY             $C8      2        1
     void JMP_ABSOLUTE();            //JMP $nnnn       $4C      3        3
     void JMP_ABSOLUTE_INDIRECT();   //JMP ($nnnn)     $6C      5        3
     void LDA_IMMEDIATE();           //LDA #$nn        $A9      2        2
     void LDA_ABSOLUTE();            //LDA $nnnn       $AD      4        3
     void LDA_ZEROPAGE();            //LDA $nn         $A5      3        2
+    void NOP();                     //NOP             $EA      2        1
+    void PHA();                     //PHA             $48      3        1
+    void PHP();                     //PHP             $08      3        1
+    
 public:
     mos6502(uint8_t (*Read)(uint16_t), void (*Write)(uint16_t, uint8_t));
     void Reset6502();
